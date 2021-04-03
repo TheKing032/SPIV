2b42bc76a96f02f7f5e4293783070d5ef23ed09d
Nebojša Bogosavljević <58832485+TheKing032@users.noreply.github.com>
2021-04-03 10:20:24 +0200

    Main_Screen

--- app/src/main/java/com/example/spiv_project/MainActivity.kt
+++ app/src/main/java/com/example/spiv_project/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.spiv_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.DialogInterface
+import kotlinx.android.synthetic.main.activity_main.*
+import android.app.AlertDialog
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val num1 = findViewById<ImageButton>(R.id.profil)
+        num1.setOnClickListener{
+            Toast.makeText(applicationContext, "HENLOOO PROFILE", Toast.LENGTH_SHORT).show()
+        }
     }
 }
